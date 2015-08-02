654e1ff9b524e3a29e597fd57219316912456656
Eugene Gurvich <gurvich@post.tau.ac.il>
2015-08-02 17:39:14 +0300

    Migration from legacy cron to tasks

--- classes/task/scheduled_queries.php
+++ classes/task/scheduled_queries.php
@@ -11,25 +11,6 @@ class scheduled_queries extends \core\task\scheduled_task {
 
     public function execute() {
         global $CFG, $DB;
-$CFG->debug=0;
-$CFG->debugdeveloper=0;
-$CFG->debugdisplay=0;
-
-        $hour = get_config('block_configurable_reports','cron_hour');
-        $min = get_config('block_configurable_reports','cron_minute');
-
-        $date = usergetdate(time());
-        $usertime = mktime($date['hours'], $date['minutes'], $date['seconds'], $date['mon'], $date['mday'], $date['year']);
-
-        $crontime = mktime($hour, $min, $date['seconds'], $date['mon'], $date['mday'], $date['year']);
-
-    #    if ( ($crontime - $usertime) < 0 ) return false;
-
-    #    $lastcron = $DB->get_field('block', 'lastcron', array('name' => 'configurable_reports'));
-    #    if (!$lastcron and ($lastcron + $this->cron < time()) ) return false;
-
-        // Starting to run...
-        //$DB->set_field('blocks','lastcron',time(), array('name' => 'configurable_reports'));
 
         require_once($CFG->dirroot."/blocks/configurable_reports/locallib.php");
         require_once($CFG->dirroot.'/blocks/configurable_reports/report.class.php');
@@ -38,7 +19,6 @@ $CFG->debugdisplay=0;
         mtrace("\nConfigurable report (block)");
 
         $reports = $DB->get_records('block_configurable_reports');
-	$DB->execute("SET SESSION wait_timeout=100000");
         if ($reports) {
             foreach ($reports as $report) {
                 // Running only SQL reports. $report->type == 'sql'
